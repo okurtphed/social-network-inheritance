a7f6b6dddc2fb959dc730cbd732196713ba886a1
okurt <okurtdev@gmail.com>
2019-05-18 20:15:37 +0000

    accounts.py and post.py files were uodated

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,32 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name =first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
+        #self.timeline = []
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+    
+    __repr__ = __str__
 
-    def add_post(self, post):
-        pass
+    def add_post(self, a_post):
+        self.posts.append(a_post)
+        return self.posts
 
     def get_timeline(self):
-        pass
+        result = []
+        if self.following:
+            for user in self.following:
+                for post in user.posts:
+                    result.append(post)
+            return result
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+        return self.following
+    
+    def __setitem__(self, key, value):
+        self.timeline[key] = value
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,55 @@
+
 from datetime import datetime
 
 # Please remove the comments and 
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
+class Post(object):
+    def __init__(self, text, user = None, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = user
+
+    def set_user(self, a_user):
+        self.user = a_user
+    
+    
+
+
+class TextPost(Post):
 #     def __init__(self, text, timestamp=None):
 #         pass
-#
-#     def set_user(self, user):
-#         pass
+
+    def __str__(self):
+        #return '@{}: "{}"\n\t{}'.format(self.user, self.text,self.timestamp.strftime("%A, %b %d, %Y"))
+    
+        return f'@{self.user}: "{self.text}"\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        self.text =text
+        self.image_url = image_url
+        self.timestamp = timestamp
+#         super().__init__(self, text)
+#         self.image_url = image_url
+        
+
+    def __str__(self):
+        return f'@{self.user}: "{self.text}"\n\t{self.image_url}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        self.text = text
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timestamp = timestamp
 
+    def __str__(self):
+        return f'@{self.user.first_name} Checked In: "{self.text}"\n\t{self.latitude}, {self.longitude}\n\t{self.timestamp.strftime("%A, %b %d, %Y")}'
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    
+    
+#will refactor the code by using super()
